25099a5e451c74e54039ac6150f6114d61c7ed94
PanagiotisPtr <lpanagiotispetridisl@gmail.com>
2019-04-01 22:45:54 +0100

    searchbar todo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ class App extends Component {
       <div className="app">
         <div className="navbar" style={{background: 'red'}}>nav</div>
         <div className="main" style={{background: 'grey'}}>
-          <div className="top-bar" style={{background: 'green'}}>search</div>
+          
+          <div className="top-bar" style={{background: 'green'}}>
+            <input className="search-box" type="search"></input>
+          </div>
+          
           <div className="row-container">
             <div className="content" style={{background: 'blue'}}>content</div>
             <div className="side-info" style={{background: 'magenta'}}>nav</div>
