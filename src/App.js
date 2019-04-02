6b06ce51e5f0452503349e5302ba0b4dbe00d8e5
PanagiotisPtr <lpanagiotispetridisl@gmail.com>
2019-04-02 01:39:54 +0100

    done with basic top-bar

--- src/App.js
+++ src/App.js
@@ -8,8 +8,23 @@ class App extends Component {
         <div className="navbar" style={{background: 'red'}}>nav</div>
         <div className="main" style={{background: 'grey'}}>
           
-          <div className="top-bar" style={{background: 'green'}}>
-            <input className="search-box" type="search"></input>
+          <div className="top-bar">
+
+            <div style={{width: '70vh'}}></div>
+
+            <div className="search-bar">
+              <i class="fas fa-search"></i>
+              <input type="search" placeholder="Search"></input>
+            </div>
+            
+            <div style={{width: '45vh'}}></div>
+
+            <div className="side-icons">
+              <i class="fas fa-plus-circle"></i>
+              <i class="fas fa-bell"></i>
+              <i class="fas fa-user-circle"></i>
+            </div>
+
           </div>
           
           <div className="row-container">
